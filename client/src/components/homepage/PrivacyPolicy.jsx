a74c2024efa965b1d0c0b267a1be637bc9af7e2a
Muhammad Haris <idris.munu@gmail.com>
2024-12-25 17:08:16 +0700

    update privacy policy

--- client/src/components/homepage/PrivacyPolicy.jsx
+++ client/src/components/homepage/PrivacyPolicy.jsx
@@ -39,12 +39,44 @@ const PrivacyPolicy = () =>{
             <p className="text-gray-700 leading-relaxed mb-6">
               You have the right to access, correct, or delete your personal data, as well as to restrict or object to its processing.
             </p>
+            <h3 className="text-2xl font-semibold mb-4">App Privacy Policy</h3>
+            <p className="text-gray-700 leading-relaxed mb-6">
+              This privacy policy applies to the SiLaBa app (hereby referred to as "Application") for mobile devices that was created by AppKamDev (hereby referred to as "Service Provider") as a Free service. This service is intended for use "AS IS".
+            </p>
+            <h3 className="text-2xl font-semibold mb-4">Data Retention Policy</h3>
+            <p className="text-gray-700 leading-relaxed mb-6">
+              The Service Provider will retain User Provided data for as long as you use the Application and for a reasonable time thereafter. If you'd like them to delete User Provided Data that you have provided via the Application, please contact them at
+              <a href="mailto:fikramulbizli@gmail.com" className="text-blue-500 ml-1 mr-1 hover:text-blue-700 transition duration-300">
+                fikramulbizli@gmail.com
+              </a>
+               and they will respond in a reasonable time.
+            </p>
+            <h3 className="text-2xl font-semibold mb-4">Security</h3>
+            <p className="text-gray-700 leading-relaxed mb-6">
+              The Service Provider is concerned about safeguarding the confidentiality of your information. The Service Provider provides physical, electronic, and procedural safeguards to protect the information processed and maintained.
+            </p>
+            <h3 className="text-2xl font-semibold mb-4">Changes</h3>
+            <p className="text-gray-700 leading-relaxed mb-6">
+              This Privacy Policy may be updated from time to time for any reason. The Service Provider will notify you of any changes by updating this page with the new Privacy Policy. Please consult this Privacy Policy regularly for any changes.
+            </p>
+            <p className="text-gray-700 leading-relaxed mb-6">
+              This privacy policy is effective as of 2024-10-10.
+            </p>
+            <h3 className="text-2xl font-semibold mb-4">Your Consent</h3>
+            <p className="text-gray-700 leading-relaxed mb-6">
+              By using the Application, you are consenting to the processing of your information as set forth in this Privacy Policy now and as amended by us.
+            </p>
             <h3 className="text-2xl font-semibold mb-4">Contact Us</h3>
-            <p className="text-gray-700 leading-relaxed">
-              If you have any questions about our privacy policy, you can contact us via email at
-              <a href="mailto:posyandukepulauanriau@gmail.com" className="text-blue-500 ml-1 hover:text-blue-700 transition duration-300">
+            <p className="text-gray-700 leading-relaxed mb-6">
+              If you have any questions about our privacy policy, you can contact us via email at 
+              <a href="mailto:posyandukepulauanriau@gmail.com" className="text-blue-500 ml-1 mr-1 hover:text-blue-700 transition duration-300">
                 posyandukepulauanriau@gmail.com
-              </a>.
+              </a>
+              or
+              <a href="mailto:fikramulbizli@gmail.com" className="text-blue-500 ml-1 hover:text-blue-700 transition duration-300">
+                fikramulbizli@gmail.com
+              </a>
+              .
             </p>
           </div>
         </div>
